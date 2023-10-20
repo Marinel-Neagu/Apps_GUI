2c587185e8474d5baf8a38561d6d5f4d20968d68
Neagu Marinel <neagumarinel2014@gmail.com>
2023-10-20 13:12:24 +0300

    add style.py and need to make more friendly

--- Calculator/style.py
+++ Calculator/style.py
@@ -1,30 +1,3 @@
 from settings import *
 import ttkbootstrap as ttk
 
-ttk.Style().configure(
-		'Result.TLabel',
-		font = (FONT, OUTPUT_FONT_SIZE)
-		)
-ttk.Style().configure(
-		'Formula.TLabel',
-		font = (FONT, NORMAL_FONT_SIZE)
-		)
-ttk.Style().configure(
-		'Number.TButton',
-		font = (FONT, NORMAL_FONT_SIZE),
-		borderwidth = 5,
-		bordercolor = 'black',
-		)
-ttk.Style().configure(
-		'Operators.TButton',
-		font = (FONT, NORMAL_FONT_SIZE),
-		borderwidth = 0,
-		background = '#F0AD4E',
-		)
-ttk.Style().configure(
-		'Symbols.TButton',
-		font = (FONT, NORMAL_FONT_SIZE),
-		background = '#4E5D6C',
-		borderwidth = 0,
-		activebackground = 'red'
-		)
