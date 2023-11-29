b3fbe50075d84ec0730c9ac8304bc615453c7597
Neagu Marinel <neagumarinel2014@gmail.com>
2023-11-29 16:52:11 +0200

    Starting the new program using customtkinter

--- Alarm_Clock/main.py
+++ Alarm_Clock/main.py
@@ -1,21 +1,44 @@
 import customtkinter as ctk
+from settings import WIDTH, HEIGHT
+from widgets import ClockFrame, AlarmClock
 
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.bind('<Alt-s>', lambda even: self.destroy())
-        self.geometry('400X400')
+        self.set_geometry(height = HEIGHT, width = WIDTH)
         self.title('')
         self.set_icon('images/empty.ico')
         
+        # set layout
+        self.rowconfigure = ()
+        self.columnconfigure = ()
+        
+        # set widgets
+        self.create_widgets()
         self.mainloop()
     
-    def set_icon(self, path):
+    def set_icon(self, path_image):
         try:
-            self.iconbitmap(path)
+            self.iconbitmap(path_image)
         except Exception:
             pass
+    
+    def set_geometry(self, width, height):
+        top = int(self.winfo_screenheight() / 2) - int(height / 2)
+        left = int(self.winfo_screenwidth() / 2) - int(width / 2)
+        self.geometry(f'{width}x{height}+{left}+{top}')
+    
+    
+    def create_widgets(self):
+        # create widgets
+        clock_frame = ClockFrame(self)
+        alarm_frame = AlarmClock(self)
+        
+        # set layout for widgets
+        clock_frame.pack(fill = 'both')
+        alarm_frame.pack(fill = 'both', expand = True)
 
 
 if __name__ == '__main__':
---
+++ Alarm_Clock/widgets.py
@@ -0,0 +1,60 @@
+import customtkinter as ctk
+import time
+
+
+class ClockFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master = parent)
+        # layout
+        self.rowconfigure((0, 1), weight = 1, uniform = 'a')
+        self.columnconfigure(0, weight = 1, uniform = 'a')
+        # set data
+        self.time_string = ctk.StringVar()
+        self.date_string = ctk.StringVar()
+        
+        # set font for labels
+        time_font = ctk.CTkFont(
+                family = 'Helvetica',
+                size = 60,
+                weight = "bold",
+                )
+        
+        date_font = ctk.CTkFont(
+                family = 'Helvetica',
+                size = 25,
+                weight = "bold",
+                )
+        # set widgets
+        
+        self.label_time = ctk.CTkLabel(
+                master = self,
+                textvariable = self.time_string,
+                font = time_font,
+                anchor = 'center'
+                )
+        
+        self.label_date = ctk.CTkLabel(
+                master = self,
+                textvariable = self.date_string,
+                anchor = 'center',
+                font = date_font,
+                )
+        
+        self.label_time.grid(row = 0, column = 0, columnspan = 2, sticky = 'ns')
+        self.label_date.grid(row = 1, column = 0, columnspan = 2, sticky = 'n')
+        
+        # logic
+        self.update_time()
+    
+    def update_time(self):
+        time_format = "%H:%M:%S"
+        date_format = "%a %B %d"
+        
+        self.time_string.set(time.strftime(time_format))
+        self.after(1000, self.update_time)
+        self.date_string.set(time.strftime(date_format))
+
+
+class AlarmClock(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master = parent, fg_color = 'red')
