925122797c216d31b20133137438f771656ee4ea
mary <neagumarinel2014@gmail.com>
2023-12-03 20:23:37 +0200

    Made the change for the spinbox

--- Alarm_Clock/widgets.py
+++ Alarm_Clock/widgets.py
@@ -108,7 +108,7 @@ class AlarmsFrame(ttk.Frame):
 class TimeChoser(ttk.Toplevel):
     
     def __init__(self, parent):
-        super().__init__(master = parent, )
+        super().__init__(master = parent)
         
         # set attributes
         
@@ -116,17 +116,39 @@ class TimeChoser(ttk.Toplevel):
         self.title('Clock')
         self.set_geometry(width = 300, height = 300)
         
-        self.rowconfigure((0, 1), weight = 1, uniform = 'a')
-        self.columnconfigure((0, 1, 2), weight = 1, uniform = 'a')
         # set data
         self.hour_int = ttk.IntVar(value = 0)
         self.minute_int = ttk.IntVar(value = 0)
         
         # set style
-        self.style_spin = ttk.Style()
-        self.style_spin.configure(
-                style = 'TButton',
-                font = ('Helvetica', 70, 'bold'),
+        self.top_level_style = ttk.Style()
+        
+        self.top_level_style.configure(
+                style = 'OK.TButton',
+                font = ('Helvetica', 10, 'bold'),
+                background = 'black',
+                anchor = 'center'
+                )
+        
+        self.top_level_style.configure(
+                style = 'CANCEL.TButton',
+                font = ('Helvetica', 10, 'bold'),
+                background = 'red',
+                anchor = 'center'
+                
+                )
+        self.top_level_style.configure(
+                style = 'PAUSE.TLabel',
+                font = ('Helvetica', 30, 'bold'),
+                anchor = 'center'
+                
+                )
+        self.top_level_style.configure(
+                style = 'TSpinbox',
+                background = 'green',
+                anchor = 'center',
+                width = 2,
+                arrowsize = 50,
                 )
         
         # create widgets
@@ -134,39 +156,65 @@ class TimeChoser(ttk.Toplevel):
         self.hour_spin = ttk.Spinbox(
                 master = self,
                 textvariable = self.hour_int,
+                font = ('Helvetica', 90, 'bold'),
                 from_ = 0,
                 to = 23
                 )
-        self.first_label = ttk.Label(master = self, text = ':')
+        self.first_label = ttk.Label(
+                master = self,
+                style = 'PAUSE.TLabel',
+                text = ':'
+                )
+        
         self.minute_spin = ttk.Spinbox(
                 master = self,
                 textvariable = self.minute_int,
+                font = ('Helvetica', 20, 'bold'),
                 from_ = 0,
-                to = 23
+                to = 59,
                 )
         
         self.ok_label = ttk.Button(
                 master = self,
                 text = 'OK',
-                style = 'TopLevel.TButton',
+                style = 'OK.TButton',
+                command = self.ok_button
                 
                 )
         self.cancel_label = ttk.Button(
                 master = self,
                 text = 'Cancel',
-                command = self.destroy,
-                style = 'TopLevel.TButton',
+                command = self.cancel_button,
+                style = 'CANCEL.TButton',
                 )
         
         # layout
-        self.hour_spin.grid(row = 0, column = 0, )
-        self.first_label.grid(row = 0, column = 1)
-        self.minute_spin.grid(row = 0, column = 2)
+        self.rowconfigure((0, 1, 2, 3), weight = 1, uniform = 'a')
+        self.columnconfigure((0, 1, 2), weight = 1, uniform = 'a')
         
-        self.ok_label.grid(row = 1, column = 0)
-        self.cancel_label.grid(row = 1, column = 2)
+        # first row
+        self.hour_spin.grid(row = 1, column = 0)
+        self.first_label.grid(row = 1, column = 1)
+        self.minute_spin.grid(row = 1, column = 2)
+        
+        # second row(the button)
+        self.ok_label.grid(row = 2, column = 0, sticky = 'news')
+        self.cancel_label.grid(row = 2, column = 2, sticky = 'news')
     
     def set_geometry(self, width, height):
         top = int(self.winfo_screenheight() / 2) - int(height / 2)
         left = int(self.winfo_screenwidth() / 2) - int(width / 2)
         self.geometry(f'{width}x{height}+{left}+{top}')
+    
+    def ok_button(self):
+        
+        if self.hour_int.get() or self.minute_int.get() != 0:
+            self.destroy()
+        else:
+            print('Sorry')
+    
+    def cancel_button(self):
+        
+        self.hour_int.set(value = 0)
+        self.minute_int.set(value = 0)
+        self.destroy()
